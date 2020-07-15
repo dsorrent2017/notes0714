4c5dd603ebf17b77261ccef1106b2492f39a7103
Owner <dsorrent7@gmail.com>
2020-07-15 06:11:46 -0400

    Updated package.json

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const readFileAsync = util.promisify(fs.readFile);
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+//var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+console.log("PORT " + PORT);
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
